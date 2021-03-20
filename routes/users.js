d81b9f336ffa627881653368d7028e5a4e6539ba
Muhammad Rifqi <muhammadrifqi.tb@gmail.com>
2021-03-20 12:56:49 +0700

    add authentication

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,95 @@
 var express = require('express');
 var router = express.Router();
-var hash = require('pbkdf2-password')()
-const User = require('../models').User;
+var bcrypt = require('bcryptjs');
+const UserModel = require('../models').User;
+const { route } = require('.');
+const jsonwebtoken = require('jsonwebtoken');
+const config = require('../config/configJwt');
+var jwtMiddleware = require('../app/middleware/jwt');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({
-    session: req.session,
-    name: req.session.user || 'no user'
+router.get('/', jwtMiddleware, async function(req, res, next) {
+  let user = await UserModel.findOne({
+    where : { id: req.userId }
   });
-});
 
-function authentication(username, password, fn) {
-  const salt = 'rifqikeren';
-
-  hash({
-    password: password,
-    salt: salt,
-  }, function (err, pass, salt, hash) {
-    if (err) return fn(err);
-    if (password == password) return fn(null, username);
-    fn(new Error('invalid password'));
-  })
-}
-
-/* POST users login. */
-router.post('/', function (req, res, next) {
-  const username = req.body.username,
-        password = req.body.password;
-
-  authentication(username, password, function(err, user) {
-    if (err) return res.json({error : 'error'});
-    req.session.regenerate(() => {
-      req.session.user = user;
-
-      res.json({
-        session: req.session.user
-      })
-    });
-
+  res.status(200).send({
+    msg: `Selamat datang ${user.username}`
   });
 });
 
 router.post('/register', async (req, res, next) => {
-    res.json(req.body);
-    // let create = await User.create(req.body);
+    // res.json(req.body);
+    let user = {
+      username : req.body.username,
+      password : bcrypt.hashSync(req.body.password, 8),
+      image: req.body.image,
+    };
 
-    // res.json(create);
+    let validation = await UserModel.findOne({
+      where : {
+        username: user.username
+      }
+    });
+
+    if (validation) {
+      res.status(200).json({
+        msg: 'Username already exists'
+      });
+
+      return false;
+    }
+
+    let userCreate = await UserModel.create(user);
+    res.status(200).json({
+      msg: 'User successfully created',
+      data : userCreate
+    });
+}); 
+
+router.post('/authentication', async (req, res, next) => {
+    let user = {
+      username : req.body.username,
+      password : req.body.password
+    };
+
+    let userData = await UserModel.findOne({
+      where : { username : user.username }
+    });
+
+    if (userData) {
+      let isValidPassword = bcrypt.compareSync(user.password, userData.password);
+      if (!isValidPassword) {
+        res.status(401).send({
+          auth: false,
+          msg: 'ERR',
+          error: 'Invalid Password'   
+        });
+        return false;
+      } 
+
+      let token = 'Bearer ' + jsonwebtoken.sign({
+        id : userData.id
+      }, config.secret, {
+        expiresIn: 1000
+      });
+
+      res.status(200).send({
+        auth: true,
+        msg: 'Success',
+        id: userData.id,
+        token: token
+      });
+      
+      return false;
+    }
+
+
+    res.status(401).send({
+      auth: false,
+      msg: 'ERR',
+      error: 'Invalid Username / Password'   
+    });
 });
 
 
